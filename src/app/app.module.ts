432aba7a5fae0c5ea57df1bcdd60df7b89cabcc4
Isa GB <isagiongo@yahoo.com.br>
2019-01-05 10:45:52 -0200

    Cria interceptor para inclusão de token nas requisições

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,5 @@
-import { ClienteService } from './../services/domain/cliente.service';
-import { StorageService } from './../services/storage.service';
-import { AuthService } from './../services/auth.service';
-import { CategoriaService } from './../services/domain/categoria.service';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
@@ -11,7 +7,12 @@ import { MyApp } from './app.component';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { ErrorInterceptorProvider } from '../interceptors/error.interceptor';
+import { CategoriaService } from '../services/domain/categoria.service';
+import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
+import { AuthService } from '../services/auth.service';
+import { StorageService } from '../services/storage.service';
+import { ClienteService } from '../services/domain/cliente.service';
+import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { ErrorInterceptorProvider } from '../interceptors/error.interceptor';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoriaService,
+    AuthInterceptorProvider,
     ErrorInterceptorProvider,
     AuthService,
     StorageService,
